dcf7b774c218da46b89c2202fe4f1699fed928e3
rollan <rollantheminion@gmail.com>
2024-10-08 14:14:25 -0700

    deployment commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,10 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
 
+if (process.env.NODE_ENV === 'production') disableReactDevTools()
+  
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>

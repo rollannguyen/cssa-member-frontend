9ef3121b8aeda35314e22c768400839eb4fa23be
rollan <rollantheminion@gmail.com>
2024-10-22 12:43:58 -0700

    updated baseURL

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import { Toaster } from 'react-hot-toast'
 import { UserContextProvider } from '../context/userContext'
 import Dashboard from './pages/Dashboard'
 
-axios.defaults.baseURL = 'http://localhost:8000'
+axios.defaults.baseURL = 'https://cssa-member-backend.vercel.app'
 axios.defaults.withCredentials = true
 
 function App() {
